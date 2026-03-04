9d20932f1ce00c050dc1e0edbb4937d37cdc2bf9
kwakseobang <iii6602@naver.com>
2026-03-04 13:16:00 +0900

    git Initial

---
+++ src/main/java/org/smu/randsome/randsomeback/RandsomeBackApplication.java
@@ -0,0 +1,13 @@
+package org.smu.randsome.randsomeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandsomeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandsomeBackApplication.class, args);
+    }
+
+}
