e4c6ae1cadd2620748a0bed89587919cc892536b
kwakseobang <iii6602@naver.com>
2026-03-06 20:08:52 +0900

    Docs: 회원가입 기능의 소셜 프로필 정보 설명 수정

--- src/main/java/org/smu/randsome/randsomeback/domain/member/service/MemberService.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/member/service/MemberService.java
@@ -27,7 +27,7 @@ public class MemberService {
      * @param emailVerificationToken 이메일 인증 토큰
      * @param credentials 회원의 계정 정보 (이메일, 비밀번호)
      * @param basicInfo 회원의 기본 정보 (이름, 생년월일 등)
-     * @param socialProfile 회원의 소셜 프로필 정보 (소셜 로그인 시)
+     * @param socialProfile 회원의 소셜 프로필 정보 (인스타그램 ID, 자기소개 등)
      * @param bankAccountInfo 회원의 은행 계좌 정보
      * @return 생성된 회원의 ID
      * */
