8e7b236c5525dc73902f98e7d0726b18737472f2
kwakseobang <iii6602@naver.com>
2026-03-08 03:49:30 +0900

    Feat: 후보자 등록 신청 시 로깅 추가

--- src/main/java/org/smu/randsome/randsomeback/domain/candidate/service/CandidateService.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/candidate/service/CandidateService.java
@@ -1,12 +1,14 @@
 package org.smu.randsome.randsomeback.domain.candidate.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.smu.randsome.randsomeback.domain.candidate.entity.CandidateRegistration;
 import org.smu.randsome.randsomeback.domain.candidate.implement.CandidateManager;
 import org.smu.randsome.randsomeback.domain.payment.implement.PaymentManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CandidateService {
@@ -22,6 +24,8 @@ public class CandidateService {
         CandidateRegistration candidateRegistration = candidateManager.apply(memberId);
 
         paymentManager.register(candidateRegistration);
+
+        log.info("[CandidateService] 매칭 후보자 등록 신청 완료 - memberId: {}", memberId);
     }
 
 }
