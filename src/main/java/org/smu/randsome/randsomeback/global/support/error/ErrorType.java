6b55e863621271919633026decb16ade197f2578
kwakseobang <iii6602@naver.com>
2026-03-08 11:45:24 +0900

    Feat: 결제 승인 상태에 따른 에러 타입 추가

--- src/main/java/org/smu/randsome/randsomeback/global/support/error/ErrorType.java
+++ src/main/java/org/smu/randsome/randsomeback/global/support/error/ErrorType.java
@@ -36,6 +36,7 @@ public enum ErrorType {
 
     // PAYMENT
     INVALID_PERSON_COUNT (HttpStatus.BAD_REQUEST, "인원 수가 유효하지 않습니다.", LogLevel.INFO),
+    NOT_ALLOW_ALREADY_APPROVED_PAYMENT (HttpStatus.BAD_REQUEST, "이미 승인된 결제는 거절이 불가능합니다.", LogLevel.INFO),
 
     // AUTH
     INVALID_EMAIL_DOMAIN        (HttpStatus.BAD_REQUEST, "상명대학교 이메일(@sangmyung.kr)만 사용 가능합니다.", LogLevel.INFO),
