b49addac499d742e951c688b6692a57a133b5ef0
y22jun <tlsdpwns0612@naver.com>
2026-03-12 17:59:09 +0900

    Chore: 사용하지 않는 어노테이션 제거

--- src/main/java/org/smu/randsome/randsomeback/admin/member/service/MemberAdminService.java
+++ src/main/java/org/smu/randsome/randsomeback/admin/member/service/MemberAdminService.java
@@ -1,7 +1,6 @@
 package org.smu.randsome.randsomeback.admin.member.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.smu.randsome.randsomeback.admin.member.controller.dto.response.MemberAdminResponse;
 import org.smu.randsome.randsomeback.domain.member.implement.MemberReader;
 import org.springframework.data.domain.Page;
@@ -9,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class MemberAdminService {
