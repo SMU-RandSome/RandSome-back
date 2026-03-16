38468144f80bbbb1db749676b0490c091bcc966a
y22jun <tlsdpwns0612@naver.com>
2026-03-17 01:19:54 +0900

    Refactor: memberId와 활성 상태 기반으로 BankAccount 조회 기능 추가

--- src/main/java/org/smu/randsome/randsomeback/domain/bankaccount/implement/BankAccountReader.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/bankaccount/implement/BankAccountReader.java
@@ -3,6 +3,7 @@ package org.smu.randsome.randsomeback.domain.bankaccount.implement;
 import lombok.RequiredArgsConstructor;
 import org.smu.randsome.randsomeback.domain.bankaccount.BankAccount;
 import org.smu.randsome.randsomeback.domain.bankaccount.repository.BankAccountJpaRepository;
+import org.smu.randsome.randsomeback.global.entity.EntityStatus;
 import org.smu.randsome.randsomeback.global.support.error.CoreException;
 import org.smu.randsome.randsomeback.global.support.error.ErrorType;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ public class BankAccountReader {
     private final BankAccountJpaRepository bankAccountJpaRepository;
 
     public BankAccount findByMemberId(Long memberId) {
-        return bankAccountJpaRepository.findByMemberId(memberId)
+        return bankAccountJpaRepository.findByMemberIdAndStatus(memberId, EntityStatus.ACTIVE)
                 .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND_BANK_ACCOUNT));
     }
 
--- src/main/java/org/smu/randsome/randsomeback/domain/bankaccount/repository/BankAccountJpaRepository.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/bankaccount/repository/BankAccountJpaRepository.java
@@ -1,12 +1,13 @@
 package org.smu.randsome.randsomeback.domain.bankaccount.repository;
 
 import org.smu.randsome.randsomeback.domain.bankaccount.BankAccount;
+import org.smu.randsome.randsomeback.global.entity.EntityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BankAccountJpaRepository extends JpaRepository<BankAccount, Long> {
 
-    Optional<BankAccount> findByMemberId(Long memberId);
+    Optional<BankAccount> findByMemberIdAndStatus(Long memberId, EntityStatus status);
 
 }
