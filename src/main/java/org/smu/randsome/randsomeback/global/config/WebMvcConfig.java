72e053b5268692f8158b740602f33ed2c729d8f1
kwakseobang <iii6602@naver.com>
2026-03-17 20:45:57 +0900

    Fix[#69]: WebMvcConfig 설정 추가 및 LoginMemberArgumentResolver 등록

---
+++ src/main/java/org/smu/randsome/randsomeback/global/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package org.smu.randsome.randsomeback.global.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.smu.randsome.randsomeback.global.annotation.resolver.LoginMemberArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
+}
