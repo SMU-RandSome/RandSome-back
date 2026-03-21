1ae9acbbfd3c31f62f9098590d5e318f04e40f55
kwakseobang <iii6602@naver.com>
2026-03-21 22:29:17 +0900

    Refactor[#106]: 인증코드 불일치 시 코드 무효화하던 로직 제거

--- src/main/java/org/smu/randsome/randsomeback/domain/auth/implement/verificationcode/VerificationCodeValidator.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/auth/implement/verificationcode/VerificationCodeValidator.java
@@ -24,7 +24,6 @@ public class VerificationCodeValidator {
             throw new CoreException(ErrorType.VERIFICATION_CODE_EXPIRED);
         }
         if (!entry.code().equals(inputCode)) {
-            codeStore.removeIfPresent(email, entry);
             throw new CoreException(ErrorType.VERIFICATION_CODE_MISMATCH);
         }
 
