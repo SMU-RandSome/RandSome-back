d923236fefdba88a10b23f867c0c2c33681516d8
kwakseobang <iii6602@naver.com>
2026-03-23 09:43:23 +0900

    File[#110]: Update package structure for SecurityConfig and SwaggerConfig

--- src/main/java/org/smu/randsome/randsomeback/global/config/SwaggerConfig.java
+++ src/main/java/org/smu/randsome/randsomeback/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.smu.randsome.randsomeback.global.swagger;
+package org.smu.randsome.randsomeback.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
