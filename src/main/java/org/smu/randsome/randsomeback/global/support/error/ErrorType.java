34c5a46f67a0e41c3afe4ac0bfa0870413dec9ea
kwakseobang <iii6602@naver.com>
2026-04-11 18:59:08 +0900

    Fix[#207]: Update duplicate ticket error message for clarity

--- src/main/java/org/smu/randsome/randsomeback/global/support/error/ErrorType.java
+++ src/main/java/org/smu/randsome/randsomeback/global/support/error/ErrorType.java
@@ -60,7 +60,7 @@ public enum ErrorType {
     NOT_FOUND_TICKET      (HttpStatus.NOT_FOUND, "티켓을 찾을 수 없습니다.", LogLevel.INFO),
     NOT_ENOUGH_TICKETS    (HttpStatus.BAD_REQUEST, "티켓이 부족합니다.", LogLevel.INFO),
     INVALID_TICKET_AMOUNT (HttpStatus.BAD_REQUEST, "유효하지 않은 티켓 수량입니다. 0보다 큰 수량을 입력해주세요.", LogLevel.INFO),
-    DUPLICATE_TICKET      (HttpStatus.CONFLICT, "이미 가입하신 회원존재하는 티켓입니다.", LogLevel.INFO),
+    DUPLICATE_TICKET      (HttpStatus.CONFLICT, "해당 회원은 이미 티켓을 보유하고 있습니다.", LogLevel.INFO),
     // PAYMENT
     INVALID_PERSON_COUNT                (HttpStatus.BAD_REQUEST, "인원 수가 유효하지 않습니다.", LogLevel.INFO),
     NOT_ALLOW_ALREADY_CONFIRMED_PAYMENT (HttpStatus.BAD_REQUEST, "이미 확정된 결제는 거절이 불가능합니다.", LogLevel.INFO),
