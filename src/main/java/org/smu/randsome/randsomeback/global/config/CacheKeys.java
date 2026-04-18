63a9642aca8e5cb2db663ad6625d560d4b058d0f
kwakseobang <iii6602@naver.com>
2026-04-18 09:04:25 +0900

    Refactor[#273]:출석 관련 캐시 키를 하드코딩에서 CacheKeys 유틸리티 메서드로 대체

--- src/main/java/org/smu/randsome/randsomeback/global/config/CacheKeys.java
+++ src/main/java/org/smu/randsome/randsomeback/global/config/CacheKeys.java
@@ -1,5 +1,6 @@
 package org.smu.randsome.randsomeback.global.config;
 
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -7,14 +8,20 @@ import lombok.NoArgsConstructor;
 public final class CacheKeys {
 
     public static final String ANNOUNCEMENTS = "announcements";
+    private static final String COUPON_EVENT = "coupon:event";
+    private static final String ATTENDANCE = "attendance";
 
     // Coupon Redis keys
     public static String couponStock(Long eventId) {
-        return "coupon:event:" + eventId + ":stock";
+        return COUPON_EVENT + ":" + eventId + ":stock";
     }
 
     public static String couponMemberLock(Long eventId, Long memberId) {
-        return "coupon:event:" + eventId + ":member:" + memberId;
+        return COUPON_EVENT + ":" + eventId + ":member:" + memberId;
+    }
+
+    public static String attendance(Long memberId, LocalDate date) {
+        return ATTENDANCE + ":" + memberId + ":" + date;
     }
 
 }
