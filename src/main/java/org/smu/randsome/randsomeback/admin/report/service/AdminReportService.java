7ce57d72f9ecc6ea39eec6ad69d2a6a4aee694ae
kwakseobang <iii6602@naver.com>
2026-04-22 11:38:04 +0900

    Feat[#291]: 회원 복구 기능 및 관련 API 제거

--- src/main/java/org/smu/randsome/randsomeback/admin/report/service/AdminReportService.java
+++ src/main/java/org/smu/randsome/randsomeback/admin/report/service/AdminReportService.java
@@ -3,7 +3,6 @@ package org.smu.randsome.randsomeback.admin.report.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.smu.randsome.randsomeback.admin.report.enums.AdminReportStatusFilter;
-import org.smu.randsome.randsomeback.domain.member.implement.MemberManager;
 import org.smu.randsome.randsomeback.domain.report.entity.Report;
 import org.smu.randsome.randsomeback.domain.report.implement.ReportManager;
 import org.smu.randsome.randsomeback.domain.report.implement.ReportReader;
@@ -15,7 +14,6 @@ public class AdminReportService {
 
     private final ReportReader reportReader;
     private final ReportManager reportManager;
-    private final MemberManager memberManager;
 
     public List<Report> findReportsByFilter(AdminReportStatusFilter statusFilter) {
         return reportReader.findAllByStatuses(statusFilter.getStatuses());
@@ -35,10 +33,6 @@ public class AdminReportService {
 
     public void rejectReport(Long reportId) {
         reportManager.markAsRejected(reportId);
-    }
-
-    public void restoreMember(Long memberId) {
-        memberManager.restore(memberId);
     }
 
 }
