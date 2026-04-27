63f056a4808ce3cc0a7f95adcd49b944f457e3db
kwakseobang <iii6602@naver.com>
2026-04-27 09:44:06 +0900

    Refactor[#319]: 쿠폰 이벤트 활성화 및 종료 스케줄을 매 시간 정각에서 매 시간 30분으로 변경

--- src/main/java/org/smu/randsome/randsomeback/domain/scheduler/CouponEventScheduler.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/scheduler/CouponEventScheduler.java
@@ -24,7 +24,7 @@ public class CouponEventScheduler {
      * 매 시간 정각마다 실행되어, 활성화 대기 중인 쿠폰 이벤트 중에서 활성화 시간이 도래한 이벤트를 활성화 상태로 변경합니다. <br>
      *
      * */
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 30 * * * *")
     @SchedulerLock(name = "activateDueEvents", lockAtMostFor = "10m", lockAtLeastFor = "1m")
     public void activateDueEvents() {
         List<CouponEvent> events = couponEventReader.findDraftEventsReadyToActivate(LocalDateTime.now());
@@ -42,7 +42,7 @@ public class CouponEventScheduler {
      * 매 시간 정각마다 실행되어, 활성화된 쿠폰 이벤트 중에서 만료 시간이 도래한 이벤트를 종료 상태로 변경합니다. <br>
      *
      * */
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 30 * * * *")
     @SchedulerLock(name = "deactivateDueEvents", lockAtMostFor = "10m", lockAtLeastFor = "1m")
     public void deactivateDueEvents() {
         List<CouponEvent> events = couponEventReader.findActiveEventsReadyToEnd(LocalDateTime.now());
