c033a316b8a53a313a671b829cb1697671a97a97
kwakseobang <iii6602@naver.com>
2026-05-02 15:35:08 +0900

    Fix[#355]: 쿠폰 만료 처리와 쿠폰 사용 동시성 오류 처리

--- src/main/java/org/smu/randsome/randsomeback/domain/coupon/repository/CouponJpaRepository.java
+++ src/main/java/org/smu/randsome/randsomeback/domain/coupon/repository/CouponJpaRepository.java
@@ -34,6 +34,7 @@ public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
             WHERE c.couponStatus = :availableStatus
               AND c.expiredAt < :now
               AND c.status = :entityStatus
+              AND c.usedAt IS NULL
             """)
     int bulkExpire(
             @Param("availableStatus") CouponStatus availableStatus,
